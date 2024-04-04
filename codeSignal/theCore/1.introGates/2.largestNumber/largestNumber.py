c9c5134586d909c59e486c284a78bb84ead99824
Luis Alejandro Soto Serafin <Luis.Alejandro.Soto.Serafin@ibm.com>
2024-04-04 12:22:33 -0600

    largestNumber

---
+++ codeSignal/theCore/1.introGates/2.largestNumber/largestNumber.py
@@ -0,0 +1,9 @@
+# time started 11:54
+def solution(n):
+    strNumber = ''
+    for i in range(n):
+        strNumber += '9'
+    largestNumber = int(strNumber)
+    return largestNumber
+
+print(solution(3))
