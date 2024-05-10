a6f51f3d2b00672874255674fb63164aa10f75bd
Luis Alejandro Soto Serafin <Luis.Alejandro.Soto.Serafin@ibm.com>
2024-05-10 12:48:36 -0600

    almostIncreasingSequence

---
+++ codeSignal/intro/2.edgeOfTheOcean/7.almostIncreasingSequence/almostIncreasingSequence.py
@@ -0,0 +1,19 @@
+def solution(sequence):
+    droppped = False
+    last = prev = min(sequence) - 1
+    for elm in sequence:
+        if elm <= last:
+            if droppped:
+                return False
+            else:
+                droppped = True
+            if elm <= prev:
+                prev = last
+            elif elm >= prev:
+                prev = last = elm
+        else:
+            prev, last = last, elm
+    return True
+
+s = [1,3,5,9,1]
+print(solution(s))
