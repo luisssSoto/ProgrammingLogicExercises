eb2591107c281150fd0d13b47cb4023b218c7ed3
AlexSoto <alejandrosotoserafin@gmail.com>
2024-07-06 19:38:45 -0600

    1480.RunningSumOf1DArray

--- leetcode/easy/1480. Running Sum of 1D Array/runningSumOf1DArray.py
+++ leetcode/easy/1480. Running Sum of 1D Array/runningSumOf1DArray.py
@@ -12,4 +12,22 @@ def runningSumOf1DArray(nums):
 
 test1 = [1,2,3,4]
 test2 = [2,3,4,5]
-print(runningSumOf1DArray(test2))
+print(runningSumOf1DArray(test1))
+
+'''Best approaches'''
+'''1.'''
+def runningSumOf1DArray(nums):
+    new_nums = nums[:1]
+    for i in range(1, len(nums)):
+        new_nums.append(nums[i] + new_nums[i - 1])
+    return new_nums
+
+print(runningSumOf1DArray(test1))
+
+'''2.'''
+def runningSumOf1DArray(nums):
+    for i in range(1, len(nums)):
+        nums[i] += nums[i - 1]
+    return nums
+
+print(runningSumOf1DArray(test1))
