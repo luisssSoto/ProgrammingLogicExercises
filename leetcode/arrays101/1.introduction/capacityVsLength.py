75c30a0364b625de0f70c9b0610c6dd6aa38442f
Luis Soto <alejandrosotoserafin@gmail.com>
2024-08-28 14:27:23 -0600

    updatingLeetcode

---
+++ leetcode/arrays101/1.introduction/capacityVsLength.py
@@ -0,0 +1,6 @@
+"""Capacity vs Length"""
+
+from whatIsAnArray import cardboard_box1
+import accessingElementsInArray
+
+print(f'carboard box 1 capacity: {len(cardboard_box1.show_collection())} and length: {cardboard_box1.get_length()}')
--- leetcode/arrays101/2.inserting items into an array/2.arrayInsertions.py
+++ leetcode/arrays101/2.inserting items into an array/2.arrayInsertions.py
@@ -24,55 +24,81 @@ print()
 how we can do it tradionatly:'''
 
 '''1. insert at the end'''
-numbers_list = [i for i in range(1, 11)]
-print(numbers_list)
+'''a) first we need to create a class that works like a limited list'''
+class CapacityError(Exception):
+    def __init__(self, message):
+        Exception.__init__(self, message)
+        
+class LimitedArray:
+    def __init__(self):
+        self.__array = [0 for x in range(6)]
+        self.__length = 0
+        
+    def insert_element(self, element, place):
+        if place < len(self.__array):
+            self.__array[place] = element
+            self.__length += 1
+        else: raise CapacityError
+    
+    def get_array(self):
+        return self.__array
+    
+'''In the case there is enough space is easy:'''
+limited_array1 = LimitedArray()
 
-'''a) first we need to create any other element at the begining of the array'''
-numbers_list.insert(0, 'Zero')
-print(numbers_list)
+# inserting elements
+for i in range(3):
+    limited_array1.insert_element(i, limited_array1._LimitedArray__length)
+
+# print each element
+for j in range(len(limited_array1.get_array())):
+    print(f"Index {j} contains {limited_array1.get_array()[j]}")
+
+# print the whole array
+print(limited_array1.get_array())
+
+# inserting another element
+limited_array1.insert_element(10, limited_array1._LimitedArray__length)
+print(limited_array1.get_array())
+
+'''But case there isn't enough space is more difficult:'''
+
+'''a) I need to fill the empty spaces..'''
+for k in range(4, 6):
+    limited_array1.insert_element('no empty', k)
+print(limited_array1.get_array())
 
 '''b) then we need to move all the elements one place towards to left'''
-for i in range(1, len(numbers_list)):
-    numbers_list[i - 1] = numbers_list[i]
-print(numbers_list)
+for l in range(len(limited_array1.get_array())-1):
+    limited_array1.get_array()[l] = limited_array1.get_array()[l+1]
+print(limited_array1.get_array())
 
 '''c) now we have enough space to add it at the end'''
-numbers_list[10] = 11
-print(numbers_list)
+limited_array1.get_array()[5] = 'full'
+print(limited_array1.get_array())
 print()
 
-
 '''2. insert at the start of an array'''
-even_numbers = [x for x in range(10) if x % 2 == 0]
-print(even_numbers)
 
-'''a) add one element at the end of an array'''
-even_numbers.append(10)
-print(even_numbers)
+'''a) shifting all the elements to the right one place'''
+for m in range(len(limited_array1.get_array())-1, 0, -1):
+    limited_array1.get_array()[m] = limited_array1.get_array()[m-1]
 
-'''b) shifting all the elements to the right one place'''
-for j in range(len(even_numbers) - 1, 0, -1):
-    even_numbers[j] = even_numbers[j - 1]
-print(even_numbers)
+print(limited_array1.get_array())
 
 '''c) insert the element at the start'''
-even_numbers[0] = -2
-print(even_numbers)
+limited_array1.get_array()[0] = "first element"
+print(limited_array1.get_array())
 print()
 
 '''3. inserting anywhere in the array'''
-odd_numbers = [x for x in range(10) if x % 2 != 0]
-print(odd_numbers)
 
-'''a) add an element at the end of the array'''
-odd_numbers[len(odd_numbers) - 1] = 11
-print(odd_numbers)
+'''a) shifting all the elements since the specific element to the right (third place)'''
+for n in range(len(limited_array1.get_array())-1, 1, -1):
+    limited_array1.get_array()[n] = limited_array1.get_array()[n-1]
+print(limited_array1.get_array())
 
-'''b) shifting all the elements since the specific element to the right (third place)'''
-for k in range(len(odd_numbers) - 1, 2, -1):
-    odd_numbers[k] = odd_numbers[k - 1]
-print(odd_numbers)
-
-'''c now the space fifth we can replace it for any value we want'''
-odd_numbers[2] = 'five'
-print(odd_numbers)
+'''c now the space third we can replace it for any value we want'''
+limited_array1.get_array()[2] = "third space"
+print(limited_array1.get_array())
+print()
