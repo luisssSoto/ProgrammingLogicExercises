4112c020ee2254368477e600e9b49d3971d123e3
AlexSoto <alejandrosotoserafin@gmail.com>
2024-09-14 10:43:48 -0600

    deletingElementsFromArray

--- leetcode/easy/26.Remove duplicates from Sorted Array/removeDuplicatesFromSortedArray.py
+++ leetcode/easy/26.Remove duplicates from Sorted Array/removeDuplicatesFromSortedArray.py
@@ -1,39 +1,72 @@
-"""Remove Duplicates From Sorted Array"""
+"""Remove Duplicated from Sorted Array"""
+#Exceeds the time limit
 def remove_duplicates(nums):
-    count = 0
-    duplicates_index = []
-    for i in range(len(nums) - 1):
-        if nums[i] == nums[i + 1]:
-            duplicates_index.append(i)
-    for index in duplicates_index:
-        del nums[index - count]
-        count += 1
+    if len(nums) <= 1:
+        return len(nums)
+    step = match = 0
+    for i in range(step, len(nums)-1):
+        if nums[step] == nums[step+1]:
+            match += 1
+            for j in range(step, len(nums)-1):
+                nums[j] = nums[j+1]
+            step -= 1
+        step += 1
+    print(f'nums: {nums}')
+    nums = nums[:len(nums)-match]
+    print(f'nums: {nums}')
     return len(nums)
-            
-test0 = [0,0,1,1,1,2,2,3,3,4]
-test1 = [1,1,2]
-print(remove_duplicates(test1))
 
-#1. input: integer array
-#   output: integer value: the length of unique values inside the modified array previously pass
+data1 = [0,0,1,1,1,2,2,3,3,4]
+data2 = [1,1,2]
+data3 = [1,2]
+data4 = [1,2,3]
+print(remove_duplicates(data1))
 
-#2. iterate nums
-#   two for loop maybe
-#   conditional statement if the element is more once delete all the copies
-#   return the lengt of the array
+'''
+1.
+input: sorted array of nums
+output sorted array but with unique elements in it
 
-#3. iterate nums with range(len(nums))
-#   iterate nums again and compare each nums[i] element with nums[j] element
-#   if nums[i] == nums[j] then...
-#   del nums[j]
-#   nums.insert(0, 0)
-#   count +=1
-#   iterate again to delete the first values according count variable
-#   return the length of the array
+2.
+iterate array length -1 
+conditional statement if i != i+1
+if so... array[step] == i+1
 
-#4. coding but be careful with delete instruction
+3.
+match = 0
+step = 1
+for i in range(len(nums)-1):
+    if nums[i] != nums[i+1]:
+        nums[step] = nums[i+1]
+        step += 1
+    else:
+        match += 1
+nums = nums[:len(nums)-match]
+return len(nums)
 
-# Above approach wasn't work, 😕 but I found another one ☺️
+4.
+happy coding!
+'''
+
+'''Better approach'''
+def remove_duplicates(nums):
+    step = 1
+    match = 0
+    for i in range(len(nums)-1):
+        if nums[i] != nums[i+1]:
+            nums[step] = nums[i+1]
+            step += 1
+        else:
+            match += 1
+    nums = nums[:len(nums)-match]
+    print(f'modyfied nums: {nums}')
+    return len(nums)
+
+data1 = [0,0,1,1,1,2,2,3,3,4]
+data2 = [1,1,2]
+data3 = [1,2]
+data4 = [1,2,3]
+print(remove_duplicates(data4))
 
 '''smarter approach and reducess time complexity'''
 def removeDuplicates(nums):
