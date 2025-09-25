eb49f673c6871182273ddd0c015dace292e3907a
AlexSoto <alejandrosotoserafin@gmail.com>
2025-09-24 22:16:12 -0500

    add exercises day 23

---
+++ leetcode/easy/163.Missing Ranges/python/missingRanges.py
@@ -0,0 +1,24 @@
+"""Missing Ranges"""
+
+def missing_ranges(nums: List[int], lower: int, upper: int) -> List[List[int]]:
+    if len(nums) == 0:
+        return [[lower, upper]]
+    matrix = []
+    index = 0
+    while lower < upper:
+        if lower < nums[index]:
+            section = [lower, nums[index] -1]
+            matrix.append(section)
+            lower = nums[index] + 1
+        else:
+            lower += 1
+        index += 1
+        if index == len(nums) and nums[-1] < upper:
+            section = [lower, upper]
+            matrix.append(section)
+            lower = upper
+    return matrix
+
+'''Complexity Analysis:
+Time Complexity: O(N)
+Space Complexity: O(1)'''
