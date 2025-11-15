2c651e05162617b0a903c1710c3fec83c5df4bf0
AlexSoto <alejandrosotoserafin@gmail.com>
2025-11-15 14:32:55 -0600

    add exercises day 54

---
+++ leetcode/9.Binary Tree/2.traverse a tree/preOrderTraversal.py
@@ -0,0 +1,53 @@
+"""Pre-Order Traversal"""
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self, root):
+        self.nodes = 0
+        self.root = root
+    def pre_order_traversal(self):
+        if not self.root:
+            return 
+        stack = [self.root]
+        tree_nodes_val = []
+        while stack:
+            current_node = stack.pop()
+            if current_node:
+                tree_nodes_val.append(current_node.val)
+                if current_node.right:
+                    stack.append(current_node.right)
+                if current_node.left:
+                    stack.append(current_node.left)
+        return tree_nodes_val
+
+
+node_f = TreeNode("F")
+node_b = TreeNode("B")
+node_a = TreeNode("A")
+node_d = TreeNode("D")
+node_c = TreeNode("C")
+node_e = TreeNode("E")
+node_g = TreeNode("G")
+node_i = TreeNode("I")
+node_h = TreeNode("H")
+
+node_f.left = node_b
+node_f.right = node_g
+node_b.left = node_a
+node_b.right = node_d
+node_g.right = node_i
+node_d.left = node_c
+node_d.right = node_e
+node_i.left = node_h
+
+binary_tree = BinaryTree(node_f)
+
+print(binary_tree)
+print(binary_tree.pre_order_traversal())
+
+        
