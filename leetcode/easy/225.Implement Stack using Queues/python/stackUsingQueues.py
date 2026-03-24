71bd6e23454c5526cc14f40c5b0690315a3dc754
AlexSoto <alejandrosotoserafin@gmail.com>
2026-03-24 06:47:07 -0500

    add exercise day 91

--- leetcode/easy/225.Implement Stack using Queues/python/stackUsingQueues.py
+++ leetcode/easy/225.Implement Stack using Queues/python/stackUsingQueues.py
@@ -20,3 +20,36 @@ class MyStack:
 
     def empty(self) -> bool:
         return True if self.length == 0 else False
+    
+class MyStack:
+
+    def __init__(self):
+        self.q1 = []
+        self.q2 = []
+
+    def push(self, x: int) -> None:
+        self.q1.append(x)
+
+    def pop(self) -> int:
+        while len(self.q1) > 1:
+            self.q2.append(self.q1.pop(0))
+        val = self.q1.pop()
+        self.q1 = self.q2
+        self.q2 = []
+        return val
+
+    def top(self) -> int:
+        return self.q1[-1]
+
+    def empty(self) -> bool:
+        return True if len(self.q1) == 0 else False
+    
+'''Complexity Analysis:
+For all the methods:
+Time Complexity: O(1)
+Space Complexity: O(1)
+
+But for the pop method:
+Time Complexity: O(N)
+Space Complexity: O(1)
+'''
